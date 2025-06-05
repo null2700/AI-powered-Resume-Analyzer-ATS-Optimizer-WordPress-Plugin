3ebecdcd6860fd3ee5f130002654b8ab1023e312
null2700 <guravsoham182@gmail.com>
2025-06-05 20:05:11 +0530

    Create form_template.php

---
+++ form-template.php
@@ -0,0 +1,5 @@
+<div class="resume-analyzer-container">
+    <textarea id="resumeInput" class="styled-textarea" rows="10" placeholder="Paste your resume here..."></textarea>
+    <button id="analyzeBtn" class="styled-button">Analyze Resume</button>
+    <pre id="output" class="styled-output"></pre>
+</div>
